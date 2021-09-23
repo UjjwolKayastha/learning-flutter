136af1122a8324694604037f4e84f1dbd04f86d3
UjjwolKayastha <uzol123@gmail.com>
2021-09-23 21:25:47 +0545

    feat: simple data mapping (map function)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:learnflutter/utils/theme.dart';
 void main() {
   runApp(MaterialApp(
     theme: appTheme(),
-    home: Home(),
+    // home: Home(),
+    home: QuoteList(),
   ));
 }
 
@@ -313,3 +314,38 @@ class _HomeState extends State<Home> {
 //     return Container();
 //   }
 // }
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken.',
+    'I have nothing to declare except my genius.',
+    'The truth is rarely pure and never simple.'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Awesome Quotes"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => Text(
+                  quote,
+                  style: TextStyle(color: AppTheme.completeBlack, fontSize: 20),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
