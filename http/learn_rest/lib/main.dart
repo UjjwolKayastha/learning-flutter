bda836c6a97549b9bc5b64a00ab638b248edf506
UjjwolKayastha <uzol123@gmail.com>
2021-10-05 16:49:22 +0545

    feat: sample implementation (services and states)

--- http/learn_rest/lib/main.dart
+++ http/learn_rest/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learn_rest/app/services/cov_api.dart';
+import 'package:learn_rest/app/services/cov_api_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +17,67 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text("HOME"),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _accessToken = "";
+  int _cases = 0;
+  int _deaths = 0;
+
+  void _updateAccessToken() async {
+    final apiService = APIService(API.sandbox());
+    final accessToken = await apiService.getAccessToken();
+    final cases = await apiService.getEndpointData(
+        accessToken: accessToken, endpoint: Endpoint.cases);
+    final deaths = await apiService.getEndpointData(
+        accessToken: accessToken, endpoint: Endpoint.deaths);
+    setState(() {
+      _accessToken = accessToken;
+      _cases = cases;
+      _deaths = deaths;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_cases != 0)
+              Text(
+                _cases.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            if (_deaths != 0)
+              Text(
+                _deaths.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _updateAccessToken,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
